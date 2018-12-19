3ac11c30ab02422b4cacd4f1e02c53cc832b44ae
Lucjan Wiśnicz <luqapo@gmail.com>
2018-12-19 18:26:17 +0100

    build

---
+++ build/precache-manifest.c07952cf3e96f1720f035713d785ce94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a125020d31fa3d445834",
+    "url": "/static/js/main.a125020d.chunk.js"
+  },
+  {
+    "revision": "7713a4131af31cbadac9",
+    "url": "/static/js/1.7713a413.chunk.js"
+  },
+  {
+    "revision": "a125020d31fa3d445834",
+    "url": "/static/css/main.3eaa2ed2.chunk.css"
+  },
+  {
+    "revision": "2b592b0a65c08ced7a08c4d7132b0820",
+    "url": "/index.html"
+  }
+];
