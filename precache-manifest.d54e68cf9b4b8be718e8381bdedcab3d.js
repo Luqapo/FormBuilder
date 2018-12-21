e2e2a113231f7191e174f334cb2aa7e5f98da979
Lucjan Wiśnicz <luqapo@gmail.com>
2018-12-21 17:10:35 +0100

    Updates

---
+++ precache-manifest.d54e68cf9b4b8be718e8381bdedcab3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4a90a5636409f1e8208",
+    "url": "/FormBuilder/static/js/runtime~main.a4a90a56.js"
+  },
+  {
+    "revision": "6dd32e8abe6123dd7872",
+    "url": "/FormBuilder/static/js/main.6dd32e8a.chunk.js"
+  },
+  {
+    "revision": "a818bd4fa4aa6bb05b4b",
+    "url": "/FormBuilder/static/js/1.a818bd4f.chunk.js"
+  },
+  {
+    "revision": "6dd32e8abe6123dd7872",
+    "url": "/FormBuilder/static/css/main.6579378c.chunk.css"
+  },
+  {
+    "revision": "802a3e2fbc2647c15f4ed070f6865c8b",
+    "url": "/FormBuilder/index.html"
+  }
+];
